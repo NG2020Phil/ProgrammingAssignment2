cf21fdcebe1ea3da21d22b05c2f302e4266075c1
NG2020Phil <68150783+NG2020Phil@users.noreply.github.com>
2020-10-31 18:24:40 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##  The following functions are used to save time when caching the inverse of large computations of data
 
 ## Write a short comment describing this function
+##This function should create a matrix object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCachematrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() (inv)
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+##This function should compute the inverse of the matrix returned by the makeCacheMatrix coding above.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cachemean <- function(x, ...)
+  {
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+  }
